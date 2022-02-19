194b30171aa178db197c398a21e7f0488c8ef326
user <sayidbek-bakhrom>
2022-02-19 14:08:08 +0500

    added forms

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
+
+from django.urls import reverse_lazy
 
 # Create your views here.
 
@@ -16,3 +18,20 @@ class BlogDetailView(DetailView):
     template_name = 'post_detail.html'
     context_object_name = 'object'
 
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['title', 'author', 'body']
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
+
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
