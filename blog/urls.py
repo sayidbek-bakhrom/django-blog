9a9636dd9166104380906a55985175bde3bd3f13
user <sayidbek-bakhrom>
2022-02-19 19:43:08 +0500

    forms and user accounts

--- blog/urls.py
+++ blog/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('post/new/', BlogCreateView.as_view(), name='post_new'),
     path('post/<int:pk>/edit/', BlogUpdateView.as_view(), name='post_edit'),
     path('post/<int:pk>/delete/', BlogDeleteView.as_view(), name='post_delete'),
+
 ]
 
